2809a48c876544b1cd617e641b00fe4e857ebc07
Peter Krauß <peter.krauss@kit.edu>
2024-05-16 17:07:44 +0200

    first version.

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Backdrop from './Backdrop.svelte'
+
+	import { fade } from 'svelte/transition'
+
+	let showTheThing = $state(false)
+</script>
+
+<button onclick={() => (showTheThing = !showTheThing)}>Toggle</button>
+
+{#if showTheThing}
+	<div
+		aria-hidden="true"
+		style="background-color: red; height: 20px;"
+		transition:fade|global={{ duration: 2000 }}
+	>
+		1
+	</div>
+	<Backdrop></Backdrop>
+{/if}
---
+++ src/routes/test/Backdrop.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition'
+</script>
+
+<div
+	aria-hidden="true"
+	style="background-color: green; height: 20px;"
+	transition:fade|global={{ duration: 2000 }}
+>
+	2
+</div>
